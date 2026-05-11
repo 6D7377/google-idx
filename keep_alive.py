0256f9a504b4b905af4dfc4a4e202abc7ef0195d
missileskywalker <121265700+6D7377@users.noreply.github.com>
2026-05-11 16:52:24 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,12 +4,12 @@ import time
 import requests
 from datetime import datetime
 
-URL = "https://8080-firebase-idx-vps-1778525463513.cluster-h4khin2zjvavytgeaz7qjiy7nw.cloudworkstations.dev/"
+URL = ""
 INTERVAL = 300  # seconds
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0",
-    "Cookie": "WorkstationJwtPartitioned=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2Nsb3VkLmdvb2dsZS5jb20vd29ya3N0YXRpb25zIiwiYXVkIjoiZmlyZWJhc2UtaWR4LXZwcy0xNzc4NTI1NDYzNTEzLmNsdXN0ZXItaDRraGluMnpqdmF2eXRnZWF6N3FqaXk3bncuY2xvdWR3b3Jrc3RhdGlvbnMuZGV2IiwiaWF0IjoxNzc4NTI4Mjc3LCJleHAiOjE3Nzg2MTQ2Nzd9.YBdW_j_asl4uTBsONDLMcvLhkBRMvyq5dDCjZLsBZerBW3gOEoKEOv4lc4qR5wkJIxtCdeq02zgsSE3XRQ-Ncevj2ODHmwjj2yd0IaKti7D04HZAvGCOyWfq66_0MorhmY1C57KVOYTjYbAWtzLoF7mt6Lc0TOGeP-zE8IkWCEEJrvdKcYKrjQ0E17_r1XKiky7oBcyDxDPWy9ii0iiarIt28dFympWA77_wHmXLaxlQU0a8Q5mA2IYDr3VBlBKj4D9Edacol0wPz0e8CpgUhsi4l4CZpiNSMJoLbowwzv1q27T5vYxnTT5GonNbMYInNJzwrpDlwXvjPElteJrcVw",
+    "Cookie": "",
 }
 
 
